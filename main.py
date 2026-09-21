fab48254627a4b65906d1b7142273d074f1f1932
ferhat7901-ai <ferhat7901@gmail.com>
2026-09-21 21:24:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -92,4 +92,16 @@ def bot_loop():
 if __name__ == "__main__":
     keep_alive() # Render için arka planda web sunucusunu açar
     bot_loop()   # MEXC tarama döngüsünü başlatır
-      
+    
+     import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot Calisiyor!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+ 
